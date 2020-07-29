afd961afd602a54e8f390d87a74609f738425014
alex <edyalex.david@gmail.com>
2020-07-30 00:21:13 +0300

    yisssssssss also dont mind the favicon.ico, it wont be used most probably

---
+++ app.py
@@ -0,0 +1,60 @@
+from mdl import UnitConversion
+from mdl import getUn, listLocator
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+import os
+
+app = Flask(__name__)
+
+
+def uper(j):
+    a = ""
+    for t in j.split("-"):
+        a += t[0].upper()
+        a += t[1:].lower()
+        a += "."
+    a = a[:-1]
+    return a
+
+
+@app.route("/")
+def index():
+    return redirect("/distance")
+
+@app.route("/<prop>", methods=["POST", "GET"])
+def propchosen(prop):
+    props = []
+    for p, d, f in os.walk("units"):
+        for i in f:
+            props.append([i, uper(i), uper(i).lower()])
+    units = getUn("units\\" + prop)
+    time = getUn("time")
+    # print(props)
+    if request.method == "POST":
+    	iv = float(request.form["iv"])
+    	iu = request.form["iu"]
+    	it = request.form["it"]
+    	ou = request.form["ou"]
+    	ot = request.form["ot"]
+    	uci = UnitConversion(iv, iu, it, ou, ot, prop)
+    	res = uci.Result
+    	selun = [iv, iu, it, ou, ot]
+    else:
+    	res = 0
+    	selun = [1.0, "", "", "", ""]
+    return render_template("index.html", props=props, res=res, selun=selun, 
+     propsel=[prop, uper(prop), uper(prop).lower()], units=[f[0] for f in units], time=[f[0] for f in time])
+
+
+# @app.route("/css/my.css")
+# def cssmine():
+#     fr = ""
+#     with open("templates/css/my.css") as f:
+#         fr += f.read()
+#     return fr
+
+
+if __name__ == '__main__':
+    app.run(port="80", host="0.0.0.0", debug=True)
--- mdl.py
+++ mdl.py
@@ -1,40 +1,58 @@
+def getUn(tC):
+	with open(tC) as f:
+		a = f.readlines()
+		for e, i in enumerate(a):
+			a[e] = i[:-1]
+		for i, j in enumerate(a):
+			a[i] = j.split(" ")
+		return a
+
+def listLocator(listtl, lt):
+	for i, j in enumerate(listtl):
+		if j == lt:
+			return i
+	return None
+
 class UnitConversion:
-    def __init__(self, sX, sXU, sTU, rXU, rTU, tC):
+	def __init__(self, sX, sXU, sTU, rXU, rTU, tC):
 
-        self.time = {}
-        self.unt = {}
-        self.sX = sX  # Input value for dx
-        self.sXU = sXU  # Input unit for dx
-        self.sTU = sTU  # Input unit for time
-        self.rXU = rXU  # Result unit for dx
-        self.rTU = rTU  # Result unit for time
-        self.Speed = sX
-        self.Result = sX
-        with open("units\\" + tC) as f:
-            a = f.readlines()
-            for e, i in enumerate(a):
-                a[e] = i[:-1]
-            for i in a:
-                b = i.split(" ")
-                self.unt[b[0]] = float(b[1])
-        with open("units\\time") as f:
-            a = f.readlines()
-            for e, i in enumerate(a):
-                a[e] = i[:-1]
-            for i in a:
-                b = i.split(" ")
-                self.time[b[0]] = float(b[1])
-        self.convertTime()
-        self.convertUnit()
+		self.time = {}
+		self.unt = {}
+		self.sX = sX  # Input value for dx
+		self.sXU = sXU  # Input unit for dx
+		self.sTU = sTU  # Input unit for time
+		self.rXU = rXU  # Result unit for dx
+		self.rTU = rTU  # Result unit for time
+		self.Speed = sX
+		self.Result = sX
+		# with open("units\\" + tC) as f:
+		#     a = f.readlines()
+		#     for e, i in enumerate(a):
+		#         a[e] = i[:-1]
+		#     for i in a:
+		#         b = i.split(" ")
+		#         self.unt[b[0]] = float(b[1])
+		h = getUn("units\\" + tC)
+		for i in h:
+			self.unt[i[0]] = float(i[1])
 
-    def convertTime(self):
-        self.Result /= self.time[self.sTU] / self.time[self.rTU]
+		# with open("time") as f:
+		#     a = f.readlines()
+		#     for e, i in enumerate(a):
+		#         a[e] = i[:-1]
+		#     for i in a:
+		#         b = i.split(" ")
+		#         self.time[b[0]] = float(b[1])
+		h = getUn("time")
+		for i in h:
+			self.time[i[0]] = float(i[1])
 
-    def convertUnit(self):
-        self.Result *= self.unt[self.sXU] / self.unt[self.rXU]
+		self.convertTime()
+		self.convertUnit()
 
-    def listLocator(self, listtl, lt):
-        for i, j in enumerate(listtl):
-            if j == lt:
-                return i
-        return None
+	def convertTime(self):
+		self.Result /= self.time[self.sTU] / self.time[self.rTU]
+
+	def convertUnit(self):
+		self.Result *= self.unt[self.sXU] / self.unt[self.rXU]
+
